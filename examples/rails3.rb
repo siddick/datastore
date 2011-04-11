541c2e32e281e0ac22df71edbc468cb2e9943f06
siddick <siddick@gmail.com>
2011-04-11 21:51:29 +0530

    new version 0.0.3

--- examples/rails3.rb
+++ examples/rails3.rb
@@ -7,8 +7,8 @@ disable_system_gems
 disable_rubygems
 bundle_path ".gems/bundler_gems"
 
-gem 'rails', '3.0.5'
-gem 'activerecord-datastore-adapter'
+gem 'rails', '3.0.6'
+gem 'activerecord-datastore-adapter', '>= 0.0.3'
 gem 'jruby-rack', '1.0.5'
 
 GEMFILE

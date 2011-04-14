071fc423d316d639a0ee2082c05bcc54ab29ebe2
siddick <siddick@gmail.com>
2011-04-14 12:59:59 +0530

    new version

--- rails3.rb
+++ rails3.rb
@@ -8,7 +8,7 @@ disable_rubygems
 bundle_path ".gems/bundler_gems"
 
 gem 'rails', '3.0.6'
-gem 'activerecord-datastore-adapter', '>= 0.0.3'
+gem 'activerecord-datastore-adapter', '>= 0.0.4'
 gem 'jruby-rack', '1.0.5'
 
 GEMFILE
